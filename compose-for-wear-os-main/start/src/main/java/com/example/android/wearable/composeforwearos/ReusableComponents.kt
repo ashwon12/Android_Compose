1e867ec15eac3ca222badf8b447411c6d938caec
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2023-05-19 17:40:36 +0900

    Use Component : Chip, ToggleChip

--- compose-for-wear-os-main/start/src/main/java/com/example/android/wearable/composeforwearos/ReusableComponents.kt
+++ compose-for-wear-os-main/start/src/main/java/com/example/android/wearable/composeforwearos/ReusableComponents.kt
@@ -35,6 +35,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,6 +47,7 @@ import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Switch
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.ToggleChip
 import androidx.wear.compose.material.ToggleChipDefaults
@@ -106,19 +109,55 @@ fun CardExample(
 
 }
 
-// TODO: Create a Chip Composable
+// 웨어에서 사용하기 적합한 material component
 @Composable
 fun ChipExample(
     modifier: Modifier = Modifier,
     iconModifier: Modifier = Modifier
 ) {
-
+    Chip(
+        modifier = modifier,
+        onClick = { /*TODO*/ },
+        label = {
+            Text(
+                text = "5 minute Meditation",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        icon = {
+            Icon(
+                imageVector = Icons.Rounded.SelfImprovement,
+                contentDescription = null,
+                modifier = iconModifier
+            )
+        })
 }
 
-// TODO: Create a ToggleChip Composable
+// Chip + 라디오버튼, 체크박스
 @Composable
 fun ToggleChipExample(modifier: Modifier = Modifier) {
-
+    var checked by remember { mutableStateOf(true) } // 상태 기억하기
+    ToggleChip(
+        modifier = modifier,
+        checked = checked,
+        toggleControl = {
+            Switch(
+                checked = checked,
+                modifier = Modifier.semantics {
+                    this.contentDescription = if (checked) "On" else "OFF"
+                }
+            )
+        },
+        onCheckedChange = { checked = it },
+        label = {
+            Text(
+                text = "Sound",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    )
 }
 
 // Function only used as a demo for when you start the code lab (removed as step 1).
