0e768f14e7e408cc47c897f8df4abd31dc8d4d3d
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2023-05-22 00:14:03 +0900

    Understanding Remember

--- compose_state/app/src/main/java/com/example/composestatus/WaterCounter.kt
+++ compose_state/app/src/main/java/com/example/composestatus/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.composestatus
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -8,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -22,12 +24,27 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         // 리컴포지션을 가능하게 하기 위한 State 타입 추가
         // 리컴포지션이 발생할 경우 0으로 초기화 되기 때문 remember을 사용하여 기존의 값을 기억하도록 한다.
         // Kotlin 의 by 키워드를 사용하여 getter와 setter 를 추가함으로써 매번 value를 언급하지 않아도 됨
-        var count by remember { mutableStateOf(0) }
+        // 리컴포저블 뿐만 아니라 앱의 요소들 ( 가로세로변경) 의 상태를 기억하기 위함 rememberSaveable 사용
+        var count by rememberSaveable { mutableStateOf(0) }
 
-        Text(text = "You've had $count glasses")
-        // 이벤트를 발생함 ( 상태 변경을 감지 하지 않았기 때문에 아무일도 일어나지 않음 )
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text(text = "물잔 추가하기")
+        if (count > 0) {
+//            var showTask by remember {
+//                mutableStateOf(true)
+//            }
+//            if (showTask){
+//                WellnessTaskItem(taskItem = "오늘 15분 걸었니?", onClose = { showTask = false})
+//            }
+            Text(text = "You've had $count glasses")
         }
+        // 이벤트를 발생함 ( 상태 변경을 감지 하지 않았기 때문에 아무일도 일어나지 않음 )
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text(text = "물잔 추가하기")
+            }
+//            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+//                Text("물잔 초기화")
+//            }
+        }
+
     }
 }
---
+++ compose_state/app/src/main/java/com/example/composestatus/WellnessTaskItem.kt
@@ -0,0 +1,30 @@
+package com.example.composestatus
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    taskItem : String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            text = taskItem
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
