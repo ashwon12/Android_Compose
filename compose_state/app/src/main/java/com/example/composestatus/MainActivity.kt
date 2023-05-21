74b57f83f79acd208ad946765a69de46bf3fa882
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2023-05-21 23:50:45 +0900

    job for Recomposition

--- compose_state/app/src/main/java/com/example/composestatus/MainActivity.kt
+++ compose_state/app/src/main/java/com/example/composestatus/MainActivity.kt
@@ -20,6 +20,13 @@ import com.example.composestatus.ui.theme.ComposeStatusTheme
  *  3. 채팅 앱에서 받은 가장 최근의 메세지
  *
  *  상태는 존재하고 이벤트는 발생한다.
+ *
+ *  컴포지션, 초기 컴포지션, 리컴포지션의 개념을 알 것.
+ *  데이터가 변경될 떄 컴포지션을 업데이트 하기 위해서 컴포저블을 다시 그리는 것을 리컴포지션이라고 칭함.
+ *  COmpose에서 틁정 상태를 읽는 리컴포지션의 예약할 수 있는 추적 시스템이 존재함.
+ *  -> State, MutableState
+ *  
+ *
  */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- compose_state/app/src/main/java/com/example/composestatus/WaterCounter.kt
+++ compose_state/app/src/main/java/com/example/composestatus/WaterCounter.kt
@@ -5,6 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -14,12 +18,15 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
     Column(modifier = modifier.padding(16.dp)) {
-        Text(text = "You've had $count glasses")
+        // 리컴포지션을 가능하게 하기 위한 State 타입 추가
+        // 리컴포지션이 발생할 경우 0으로 초기화 되기 때문 remember을 사용하여 기존의 값을 기억하도록 한다.
+        // Kotlin 의 by 키워드를 사용하여 getter와 setter 를 추가함으로써 매번 value를 언급하지 않아도 됨
+        var count by remember { mutableStateOf(0) }
 
+        Text(text = "You've had $count glasses")
         // 이벤트를 발생함 ( 상태 변경을 감지 하지 않았기 때문에 아무일도 일어나지 않음 )
-        Button(onClick = {count++}, Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text(text = "물잔 추가하기")
         }
     }
