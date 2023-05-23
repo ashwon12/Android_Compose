9bf24322827ac594ef498230209d29d706b347ca
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2023-05-23 16:49:53 +0900

    Change List to MutableStateList

--- compose_state/app/src/main/java/com/example/composestatus/WellnessScreen.kt
+++ compose_state/app/src/main/java/com/example/composestatus/WellnessScreen.kt
@@ -2,6 +2,8 @@ package com.example.composestatus
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 
 
@@ -11,7 +13,17 @@ fun WellnessScreen(
 ){
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+
+        // rememberSaveable()를 사용하여 WellnessScreen에 목록을 저장하려고 하면 런타임 예외가 발생
+        val list = remember {
+            getWellnessTasks().toMutableStateList()
+        }
+        WellnessTasksList(list = list, onCloseTask = {
+                task -> list.remove(task)
+        })
     }
 
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
--- compose_state/app/src/main/java/com/example/composestatus/WellnessTaskList.kt
+++ compose_state/app/src/main/java/com/example/composestatus/WellnessTaskList.kt
@@ -2,25 +2,28 @@ package com.example.composestatus
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 // 리스트 값 업데이트
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
 
-
-// 리싸이클러뷰를 만들기 위한 껍데기
+// 리싸이클러뷰를 만들기 위한 껍데
 @Composable
 fun WellnessTasksList(
     modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit
 ) {
+    val scrollState = rememberLazyListState()
     LazyColumn(
-        modifier = modifier
+        modifier = modifier,
+        state = scrollState
     ) {
-        items(list) { task ->
-            WellnessTaskItem(taskItem = task.label)
+        items(items =list,
+        key = {task -> task.id}) { task ->
+            WellnessTaskItem(taskItem = task.label, onClose = { onCloseTask(task) })
         }
     }
 }
