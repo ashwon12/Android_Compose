6d8b44c4571ed3cae96264f3e458c3bd91a4e5a4
asheon12 <55980680+ashwon12@users.noreply.github.com>
2025-01-31 23:42:21 +0900

    [init] meterial2 theme

--- basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -1,34 +1,111 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.example.cupcake
+package com.yapp.feature.login
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.example.cupcake.ui.theme.CupcakeTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yapp.core.designsystem.component.button.solid.YappSolidPrimaryButtonXLarge
+import com.yapp.core.designsystem.component.input.text.YappInputTextLarge
+import com.yapp.core.designsystem.theme.YappTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-        setContent {
-            CupcakeTheme {
-                CupcakeApp()
+
+@Composable
+fun LoginScreen() {
+    var defaultInputValue by remember { mutableStateOf("") }
+    var errorInputText by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp),
+        contentAlignment = Alignment.TopStart
+    ) {
+
+        Column {
+            Text(
+                modifier = Modifier.padding(top = 72.dp, bottom = 32.dp),
+                text = "당신의 작은 아이디어가\n" +
+                        "세상을 바꿉니다.",
+                style = YappTheme.typography.title2Bold,
+            )
+            Image(
+                painter = painterResource(id = R.drawable.illust_login_yappu),
+                contentDescription = null
+            )
+            YappInputTextLarge(
+                label = "이메일",
+                value = defaultInputValue,
+                onValueChange = { defaultInputValue = it },
+                placeholder = "YAPP@email.com",
+            )
+
+            Spacer(Modifier.height(16.dp))
+            YappInputTextLarge(
+                label = "비밀번호",
+                password = defaultInputValue,
+                onPasswordChange = { defaultInputValue = it },
+                placeholder = "••••••••"
+            )
+
+            Spacer(Modifier.height(24.dp))
+
+            YappSolidPrimaryButtonXLarge(
+                modifier = Modifier.fillMaxWidth(),
+                text = "로그인",
+                enable = false,
+                onClick = {}
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp)
+            ) {
+                HorizontalDivider(
+                    modifier = Modifier.weight(1f),
+                    color = Color.LightGray,
+                    thickness = 1.dp
+                )
+                Text(
+                    text = "또는",
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                    color = Color.LightGray,
+                    style = YappTheme.typography.caption1Medium
+                )
+                HorizontalDivider(
+                    modifier = Modifier.weight(1f),
+                    color = YappTheme.colorScheme.lineNormalNormal,
+                    thickness = 1.dp
+                )
             }
+
         }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginScreenPreview() {
+    YappTheme {
+        LoginScreen()
     }
 }
